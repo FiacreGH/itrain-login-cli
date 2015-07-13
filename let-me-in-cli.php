6e3cd126b93b35ad62647d577c7242f99445eebc
Fiacre Sankara <fiacre2000@gmail.com>
2015-07-13 19:15:27 +0000

    Putting together the "let-me-in-3x-cli.php" and the "let-me-in-cli.php" in one file

--- let-me-in-cli.php
+++ let-me-in-cli.php
@@ -22,3 +22,29 @@ if (!empty($user)) {
 }
 $logger->log('Non, vous restez dehors!!!');
 
+/**
+ * La variante qui propose 3 essaies ;)
+ */
+include('Classes/Database.php');
+include('Classes/Logger.php');
+//connexion à la bd
+$database = new Ecodev\Database('localhost', 'root', 'root');
+$database->connect('authentificate');
+$logger = new Logger();
+$isAuthenticated= FALSE;
+for ($i = 1; $i <= 3 /* ||$isAuthenticated */; $i++) {
+	$logger->log('Bonjour, qui êtes-vous?');
+	$username = trim(fgets(STDIN));
+	$logger->log('Et votre passe?');
+	$password = trim(fgets(STDIN));
+	$hachedPassword = md5($password);
+	$user = $database->selectOne('SELECT * FROM user where username = "' . $username . '" AND passwd = "' . $hachedPassword.'"LIMIT 1 ');
+	if (!empty($user)){
+		$logger->log('Ok, welcome vous êtes dedans!!!');
+		//$isAuthenticated = TRUE;
+		exit();
+	}
+	$logger->log('ERREUR,' . $i .'ème essaie!!!');
+}
+$logger->log('Non, vous restez dehors!!!');
+
